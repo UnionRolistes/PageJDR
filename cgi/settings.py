208faf4fd72bf01cc5fd3edae7ec4467913352f3
Youssef <agoussalyo@eisti.eu>
2021-07-07 20:31:04 +0200

    Changed discord message logo

--- cgi/settings.py
+++ cgi/settings.py
@@ -1,4 +1,4 @@
 command_prefix = "$"
-logo_url = "https://cdn.discordapp.com/attachments/688340383117082733/688732386820620351/UR-logo2.png"
+logo_url = "https://cdn.discordapp.com/attachments/652207549168484352/862019020516622377/ur.png" 
 bot_token = 'ODU2NDkyNjM3NzAzMzcyODEx.YNB07Q.QM8tx3yyo3p6C-80QcDR4iD6ldI'
 webhook_url = 'https://discord.com/api/webhooks/856626728076443668/gtNCxdOhWUu7qcc0HyYp10085j0qY_f8cV_hm-0vctzVqoUdlEpIErQKdTDoWemIl3aq'
