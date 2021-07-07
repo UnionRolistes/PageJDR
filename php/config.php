fe6d043a35cd27fa2e14c327b0b974f08195ac1c
MoonLyss <shaitan.lyssm@gmail.com>
2021-07-07 18:10:16 +0200

    add config.php

---
+++ php/config.php
@@ -0,0 +1,6 @@
+<?php
+define("CLIENT_ID", "");
+define("CLIENT_SECRET", "");
+define("REDIRECT_URI","http://urplanning.unionrolistes.fr");# . substr(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), 0, -1));
+define("WEBHOOK_URI", "");
+?>
