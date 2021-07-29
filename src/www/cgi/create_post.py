6514663a98f6b4a785e6b724498e793899567e95
MoonLyss <shaitan.lyssm@gmail.com>
2021-07-29 15:48:02 +0200

    Fix

--- src/www/cgi/create_post.py
+++ src/www/cgi/create_post.py
@@ -1,17 +1,22 @@
 #!/opt/virtualenv/URBot/bin/python
+from urpy import utils
+#utils.html_header_webhook_not_supplied()
+import cgi
+import cgitb
+cgitb.enable()
+
 from os import path
 
-from discord import Webhook, RequestsWebhookAdapter
+from discord import Webhook, RequestsWebhookAdapter, AsyncWebhookAdapter
 import discord
 from settings import logo_url
 from const import *
-import cgi
-import cgitb
+
 from urpy.xml import Calendar
 from urpy import utils
 
 # Logging
-cgitb.enable(display=1)  # , logdir="/usr/local/log/cgi")
+#cgitb.enable()  # , logdir="/usr/local/log/cgi")
 
 
 def get_payload(form) -> str:
@@ -35,7 +40,7 @@ def get_payload(form) -> str:
         players=players,
         length=form.getvalue('jdr_length'),
         pseudoMJ=f"<@{form.getvalue('user_id')}> [{form.getvalue('pseudo')}]",  # TODO handle server nicknames
-        system=form.getvalue('jdr_system') + form.getvalue('jdr_system_other'),
+        system=form.getvalue('jdr_system') if form.getvalue('jdr_system') else form.getvalue('jdr_system_other'),
         minors_allowed=minorsAllowed_to_str[int(form.getvalue('jdr_pj'))],
         platforms=" ".join(form.getlist('platform')),
         details=form.getvalue('jdr_details')
@@ -45,25 +50,28 @@ def get_payload(form) -> str:
     return payload
 
 
-def get_webhook_url() -> str:
+def get_webhook_url(form) -> str:
     return form.getvalue('webhook_url')
 
+async def main():
+    form = cgi.FieldStorage()        
+    embed=discord.Embed(description=get_payload(form), type="rich").set_thumbnail(url=logo_url)
 
-if __name__ == '__main__':
-    form = cgi.FieldStorage()
-    webhook_url = form.getvalue('webhook_url')
-    if webhook_url:
-        webhook = Webhook.from_url(get_webhook_url(), adapter=RequestsWebhookAdapter())
-        msg = webhook.send(
-            "",
-            allowed_mentions=discord.AllowedMentions(users=True),
-            embed=discord.Embed(description=get_payload(form), type="rich").set_thumbnail(url=logo_url),
-        )
-        calendar = Calendar(path.abspath('../Calendar/data/events.xml'))
-        calendar.add_event(form, msg)  # TODO maybe move webhook processing to urpy
-        calendar.save()
-        # Redirects to main page
-        utils.html_header_relocate(f"http://urplanning.unionrolistes.fr?webhook={get_webhook_url()}")
-    else:
-        utils.html_header_webhook_not_supplied()
+    calendar = Calendar(path.abspath('../Calendar/data/events.xml'))
+    await calendar.add_event(form, embed)  # TODO maybe move webhook processing to urpy
+    calendar.save()
+    # Redirects to main page
+    utils.html_header_relocate(f"http://babylyss.com")
 
+
+
+try:
+    #utils.html_header_content_type()
+    import os
+    #print(os.listdir("../Calendar"))
+    import asyncio
+    asyncio.run(main())
+except Exception as e:
+    print("Content-Type: text/html")
+    print()
+    raise e
