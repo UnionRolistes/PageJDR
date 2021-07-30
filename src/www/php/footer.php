f5babbbaf82fbc79c75bc02766115c34cace49aa
Youssef <agoussalyo@eisti.eu>
2021-07-30 17:09:12 +0200

    Minor fixes

--- src/www/php/footer.php
+++ src/www/php/footer.php
@@ -1,7 +1,8 @@
 <footer>           
     <div class="titleCenter">
         <h5><a rel="license" href="http://creativecommons.org/licenses/by/2.0/fr/">
-            <img alt="Licence Creative Commons" style="border-width:0" src="img/ccby.png"/>
+            <?php define( 'ROOT_URI', '/Web_Planning/src/www/') // la racine est /var/www/html sur le serveur?>
+            <img alt="Licence Creative Commons" style="border-width:0" src="<?=ROOT_URI?>img/ccby.png"/>
         </a> 
         &copy; 2021 Page créée par <a href="http://unionrolistes.fr/">l'Union Des Rôlistes</a>. Association et marque deposée. Version BETA</h5>
         Ce(tte) œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by/2.0/fr/">Licence Creative Commons Attribution 2.0 France</a>
